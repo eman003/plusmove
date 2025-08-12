51617c4ee668248de68e30aa4a102d300b6c525c
eman003 <fulufhelo.mukwevho6@gmail.com>
2025-08-12 21:29:21 +0200

    Address - Created an address model, migration, factory and seeder - Added a polymorphic relationship to user

---
+++ database/migrations/2025_08_12_190915_create_addresses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('addressable');
+            $table->string('name')->nullable();;
+            $table->string('address_line_1');
+            $table->string('address_line_2')->nullable();
+            $table->string('suburb');
+            $table->string('city');
+            $table->string('province');
+            $table->string('postal_code');
+            $table->string('country');
+            $table->boolean('is_default')->default(true);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
