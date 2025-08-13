a0320be8f51fe72b4a4c04509a34a341ab934c1f
eman003 <fulufhelo.mukwevho6@gmail.com>
2025-08-13 18:47:50 +0200

    City - created city model, migration, seeder and factory

---
+++ database/factories/CityFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\City>
+ */
+class CityFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->city(),
+            'province' => fake()->randomElement([
+                'Limpopo', 'Gauteng', 'Mpumalanga', 'Kwazulu Natal', 'North West', 'Northern Cape',
+                'Eastern Cape', 'Western Cape', 'Free State'
+            ]),
+        ];
+    }
+}
