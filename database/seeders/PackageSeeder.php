b0bc105c4e30764e37558171abe0b6339cbc89ea
eman003 <fulufhelo.mukwevho6@gmail.com>
2025-08-15 08:27:14 +0200

    Package
    - created a package resource
    - list package
    - create a package
    - update a package
    - delete a package

--- database/seeders/PackageSeeder.php
+++ database/seeders/PackageSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\V1\Customer;
+use App\Models\V1\Delivery;
+use App\Models\V1\Package;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +15,9 @@ class PackageSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Package::factory()->count(rand(10, 50))->create([
+            'customer_id' => fake()->randomElement(Customer::all()->pluck('id')->toArray()),
+            'delivery_id' => fake()->randomElement([...Delivery::all()->pluck('id')->toArray(), null]),
+        ]);
     }
 }
